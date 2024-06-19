6ccea4f437a0f903c446daf36f4032a51edc7ec4
ramune314159265 <ramune314159265@gmail.com>
2024-06-19 21:34:57 +0900

    設定出来るように

---
+++ src/main/java/ramune314159265/velocityserverlinks/LinkConfig.java
@@ -0,0 +1,6 @@
+package ramune314159265.velocityserverlinks;
+
+public class LinkConfig {
+	public String name;
+	public String url;
+}
--- src/main/java/ramune314159265/velocityserverlinks/ServerLinks.java
+++ src/main/java/ramune314159265/velocityserverlinks/ServerLinks.java
@@ -1,15 +1,25 @@
 package ramune314159265.velocityserverlinks;
 
 import com.google.inject.Inject;
+import com.moandjiezana.toml.Toml;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.util.ServerLink;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.slf4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Plugin(
 		id = "velocity-serverlinks",
@@ -23,20 +33,52 @@ public class ServerLinks {
 
 	@Inject
 	private final ProxyServer server;
+	private final Path configFolder;
 
 	@Inject
-	public ServerLinks(ProxyServer server, Logger logger) {
+	public ServerLinks(ProxyServer server, Logger logger, @DataDirectory Path configFolder) {
 		this.server = server;
+		this.configFolder = configFolder;
 
 		ServerLinks.logger = logger;
 		ServerLinks.ServerLinkList = new ArrayList<>();
 	}
 
+	private void loadConf() {
+		File folder = configFolder.toFile();
+		File configFile = new File(folder, "links.toml");
+		if (!configFile.getParentFile().exists()) {
+			configFile.getParentFile().mkdirs();
+		}
+
+		if (!configFile.exists()) {
+			try (InputStream input = ServerLink.class.getResourceAsStream("/" + configFile.getName())) {
+				if (input != null) {
+					Files.copy(input, configFile.toPath());
+				} else {
+					configFile.createNewFile();
+				}
+			} catch (IOException e) {
+				logger.error(e.toString());
+			}
+		}
+
+		Toml configToml = new Toml().read(configFile);
+		ServerLinks.ServerLinkList.clear();
+		List<HashMap<String,String>> links = configToml.getList("links");
+		links
+				.forEach(link -> {
+					try {
+						ServerLinks.ServerLinkList.add(ServerLink.serverLink(ServerLink.Type.valueOf(link.get("name")), link.get("url")));
+					} catch (IllegalArgumentException e) {
+						ServerLinks.ServerLinkList.add(ServerLink.serverLink(MiniMessage.miniMessage().deserialize(link.get("name")), link.get("url")));
+					}
+				});
+	}
+
 	@Subscribe
 	public void onProxyInitialization(ProxyInitializeEvent event) {
-		logger.info("event registering");
+		loadConf();
 		server.getEventManager().register(this, new PlayerListener());
-
-		ServerLinks.ServerLinkList.add(ServerLink.serverLink(ServerLink.Type.valueOf("WEBSITE"), "https://example.com"));
 	}
 }
